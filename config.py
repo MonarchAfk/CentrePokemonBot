aea33958670e097217fb15fb15f27c8849956b70
MonarchAfk <sergentfeu@gmail.com>
2026-07-25 22:24:55 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+TOKEN = os.getenv("MTQ3NTkxOTQ2MTM5MDIyMTQyNQ.G6BLJx.yDYb6foDCY6fJvxMHlg2-dIzkowBOqTOWKclPE")
+
+SERVER_NAME = "🏯〡Centre Pokémon"
+
+COLOR = 0xC8A2FF
+
+NEWS_CHANNEL_ID = 0
+
+WELCOME_IMAGE = "https://i.imgur.com/example.png"
+
+PREFIX = "!"
